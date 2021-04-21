c2817e375673bf20905b51e17facabd68892e32b
Alma-byte <legnaneleh@gmail.com>
2021-04-21 12:39:50 -0500

    guarda en firebase e imprime

--- src/Pages/NoteEdit.js
+++ src/Pages/NoteEdit.js
@@ -1,32 +1,31 @@
 import React, {useState} from "react";
 
 const NoteEdit = (props) =>{  
-  console.log(props)
   const inicialStatateValues = {
-    id:"",
+    name:"",
     note:"",
   };
 
   const [values, setValues] = useState(inicialStatateValues);
   
   const Saveinputchange = (e) =>{
-    const { id, value} = e.target;
-    setValues({ ...values, [id]: value });
+    const { name, value} = e.target;
+    setValues({ ...values, [name]: value });
   };
 
   const savefarebase = (e) => {
     e.preventDefault();
-    console.log (values);
-    props.db.collection('notes').add(values)
+    props.db.collection('notes').add(values);
+    
   };
 
     return (
           <form className="note-body" onSubmit={savefarebase}>
             <div className= "note-group">
-            <input type="text" className= "note-control" placeholder="NOTA" id="boxnote" onChange= {Saveinputchange} />
+            <input type="text" className= "note-control" placeholder="NOTA"  name="name" onChange= {Saveinputchange} />
             </div>
             <div className= "note-text">
-              <textarea id="description" cols="20" rows="10" onChange= {Saveinputchange}></textarea>
+              <textarea name="note" cols="20" rows="10" onChange= {Saveinputchange} ></textarea>
             </div>
             <button className="btnsave">
               CREAR NOTA
--- src/Pages/Notee.js
+++ src/Pages/Notee.js
@@ -1,15 +1,39 @@
-import React from "react";
-import NoteEdit from "./NoteEdit"
+import React, {useEffect, useState} from "react";
 import {db} from "../firebase"
 
 const Note = () => {
 
+  const [Notes, setNotes] = useState([]);
+
+  const getnotes = async () => {
+     db.collection('notes').onSnapshot ((querySnapshot)=> {
+      const docs = [];
+        querySnapshot.forEach(doc => {
+          docs.push({...doc.data(),id: doc.id});
+       });
+       setNotes(docs);
+      });
+  };
+
+  useEffect(() => {
+    getnotes();
+    console.log(' getting data ...')
+  },[]);
+
     return (
       <div>
-        <NoteEdit db={db} />
-        <h1>Nota</h1>
+        <div className="col-md-8">
+          {Notes.map(nota =>(
+            <div className="card">
+              <div className="card-body">
+                <h2>{nota.name} </h2>
+                <p>{nota.note} </p>
+        </div>
       </div>
+          ))}
+    </div>
+  </div>
     );
-};
+  };
 
 export default Note;
